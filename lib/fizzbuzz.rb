e0b97d8459e22a5df770383fec1b3ec6cb69d46a
Jacob Cho <jacobcho@live.com>
2021-09-13 05:53:31 -0600

    14 tests put on rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(number)
-  if number == 5
-    "buzz"
-  elsif number == 15
+  if number % 5 == 0 && number % 3 == 0
     "fizzbuzz"
+  elsif number % 5 == 0
+    "buzz"
   elsif number % 3 == 0
     "fizz"
   else
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,9 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
@@ -9,6 +12,9 @@ describe 'fizzbuzz' do
   end
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "buzz" when passed 20' do
+    expect(fizzbuzz(20)).to eq 'buzz'
   end
   it 'returns "23" when passed 23' do
     expect(fizzbuzz(23)).to eq 23
@@ -19,8 +25,23 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
-   it 'returns "fizz" when passed 9' do
+  it 'returns "fizz" when passed 9' do
     expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed 60' do
+    expect(fizzbuzz(60)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed 2085' do # large multiple of 3 && 5
+    expect(fizzbuzz(2085)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizz" when passed 287046' do # large multiple of 3
+    expect(fizzbuzz(287046)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 31780' do # large multiple of 5
+    expect(fizzbuzz(31780)).to eq 'buzz'
   end
 end
 
